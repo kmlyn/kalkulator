abf95eba1396a0678d6674965d7e91e415c605c6
Krzysztof Młynarczyk <11165215+kmlyn@users.noreply.github.com>
2020-09-28 11:12:22 +0200

    First commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla;
+
+public class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+}
---
+++ src/main/java/com/kodilla/Main.java
@@ -0,0 +1,10 @@
+package com.kodilla;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        com.kodilla.Calculator calculator = new com.kodilla.Calculator();
+        System.out.println("2.5 - 3 = " + calculator.subtract(2.5, 3));
+    }
+}
